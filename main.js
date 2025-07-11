c92509942ee2024bfc229b291c9443caa9f20c1a
kashishs1993 <kashishs1993@gmail.com>
2025-07-11 17:07:23 +0200

    video change

--- main.js
+++ main.js
@@ -13,14 +13,14 @@ document.addEventListener('DOMContentLoaded', () => {
     const {renderer, scene, camera} = mindarThree;
     
     //video
-    const video = await loadVideo("./videos/video1.mp4");
+    const video = await loadVideo("./videos/video4.mp4");
     const texture = new THREE.VideoTexture(video);
 
-    const geometry = new THREE.PlaneGeometry(1, 1);
-    const material = createChromaMaterial(texture, 0x000000); //chroma
+    // const geometry = new THREE.PlaneGeometry(1, 1);
+    // const material = createChromaMaterial(texture, 0x000000); //chroma
     
-    // const geometry = new THREE.PlaneGeometry(2, 1080/(1920/2));
-    // const material = new THREE.MeshBasicMaterial({map: texture});
+    const geometry = new THREE.PlaneGeometry(2, 1080/(1920/2));
+    const material = new THREE.MeshBasicMaterial({map: texture});
     const plane = new THREE.Mesh(geometry, material);
 
     const anchorVid = mindarThree.addAnchor(0);
